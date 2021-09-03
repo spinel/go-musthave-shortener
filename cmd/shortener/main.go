d54e6d0a969dccadd9582a0be7053b2898a41ae6
Vladimir Kaydalin <exfinancier@gmail.com>
2021-09-03 19:09:19 +0300

    graceful shutdown

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,21 +1,43 @@
 package main
 
 import (
-	"log"
+	"context"
 	"net/http"
+	"os"
+	"os/signal"
+	"time"
 
-	"github.com/spinel/go-musthave-shortener-tpl/internal/app/repository"
-	"github.com/spinel/go-musthave-shortener-tpl/internal/app/router"
+	"github.com/spinel/go-musthave-shortener/internal/app/repository"
+	"github.com/spinel/go-musthave-shortener/internal/app/router"
 )
 
 func main() {
+	// Init repo.
 	repo, err := repository.New()
 	if err != nil {
 		panic(err)
 	}
 
+	server := &http.Server{Addr: ":8080", Handler: router.NewRouter(repo)}
 	go func() {
-		http.Handle("/", router.Router(repo))
+		if err := server.ListenAndServe(); err != nil {
+			panic(err)
+		}
 	}()
-	log.Fatal(http.ListenAndServe(":8080", nil))
+
+	// Setting up signal capturing
+	stop := make(chan os.Signal, 1)
+
+	// Set os signals to the chan
+	signal.Notify(stop, os.Interrupt)
+
+	// Waiting for SIGINT (pkill -2)
+	<-stop
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := server.Shutdown(ctx); err != nil {
+		panic(err)
+	}
+	// Wait for ListenAndServe goroutine to close.
 }
