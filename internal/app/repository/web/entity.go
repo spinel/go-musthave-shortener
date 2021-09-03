e26aa6abd2a45398deaf3418a28726ff24333f32
Vladimir Kaydalin <exfinancier@gmail.com>
2021-09-03 19:11:21 +0300

    memory field unexported, check added to GetEntityBy method

--- internal/app/repository/web/entity.go
+++ internal/app/repository/web/entity.go
@@ -1,36 +1,40 @@
 package web
 
 import (
-	"github.com/spinel/go-musthave-shortener-tpl/internal/app/model"
+	"errors"
+
+	"github.com/spinel/go-musthave-shortener/internal/app/model"
 )
 
 // EntityRepo is a repo for objects.
 type EntityRepo struct {
-	Memory map[string]model.Entity
+	memory map[string]model.Entity
 }
 
 // NewEntityRepo is a builder of repository.
 func NewEntityRepo(db map[string]model.Entity) *EntityRepo {
 	var repo EntityRepo
-	repo.Memory = db
+	repo.memory = db
 
 	return &repo
 }
 
 // GetEntityBy - retrive entity by id.
-func (repo *EntityRepo) GetEntityBy(id string) (model.Entity, error) {
-	entity := repo.Memory[id]
-	return entity, nil
+func (repo *EntityRepo) GetEntityBy(id string) (*model.Entity, error) {
+	if entity, ok := repo.memory[id]; ok {
+		return &entity, nil
+	}
+	return nil, errors.New("not found")
 }
 
 // SaveEntity - saves given model by id.
 func (repo *EntityRepo) SaveEntity(id string, entity model.Entity) error {
-	repo.Memory[id] = entity
+	repo.memory[id] = entity
 	return nil
 }
 
 // IncludesCode check if id exists in repo.
 func (repo *EntityRepo) IncludesCode(id string) bool {
-	_, result := repo.Memory[id]
-	return result
+	_, found := repo.memory[id]
+	return found
 }
