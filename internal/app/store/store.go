3cc9829642f5ebe643224917044d4f29cdd0e4d2
Vladimir Kaydalin <exfinancier@gmail.com>
2021-09-09 17:21:45 +0300

    store mutex added

--- internal/app/store/store.go
+++ internal/app/store/store.go
@@ -3,11 +3,13 @@ package store
 import (
 	"encoding/gob"
 	"os"
+	"sync"
 
 	"github.com/spinel/go-musthave-shortener/internal/app/model"
 )
 
 type Store struct {
+	mu          sync.Mutex
 	gobFileName string
 	gobFile     *os.File
 	decoder     *gob.Decoder
@@ -25,17 +27,21 @@ func NewStore(gobFileName string) *Store {
 
 // GetData retrives data from gob file
 func (s *Store) GetData() (map[string]model.Entity, error) {
+	s.mu.Lock()
 	memory := make(map[string]model.Entity)
 	s.decoder.Decode(&memory)
+	defer s.mu.Unlock()
 
 	return memory, nil
 }
 
 // SaveData save memory storage to gob file
 func (s *Store) SaveData(memory map[string]model.Entity) error {
+	s.mu.Lock()
 	file, _ := os.Create(s.gobFileName)
 	defer file.Close()
 	encoder := gob.NewEncoder(file)
+	defer s.mu.Unlock()
 
 	return encoder.Encode(memory)
 }
