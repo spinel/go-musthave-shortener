8cc18b533ea86415396dc5aab431fa014af045bb
Vladimir Kaydalin <exfinancier@gmail.com>
2022-03-06 20:21:41 +0300

    Revert "env update"
    This reverts commit bf1e5bd1c1478b86dc2c94150c686161bf28e4df.

--- cmd/shortener/env.sh
+++ cmd/shortener/env.sh
@@ -3,6 +3,6 @@ export SERVER_ADDRESS=localhost:8080
 export BASE_URL=http://localhost:8080
 export FILE_STORAGE_PATH=urls.gob
 export DATABASE_DSN=postgres://postgres:docker@localhost:5439/postgres?sslmode=disable
-export PG_MIGRATIONS_PATH=file://internal/app/repository/pg/migrations
+export PG_MIGRATIONS_PATH=file://../../internal/app/repository/pg/migrations
 export COOKIE_SECRET_KEY=102703av0grv8n4l
-export BATCH_QUEUE_SIZE=1
+export BATCH_QUEUE_SIZE=10
